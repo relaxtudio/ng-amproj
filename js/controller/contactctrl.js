13b16a03c4ee4d08cd6d605c48325c08cf74c452
renanonymous21 <rendy.kr21@gmail.com>
2017-10-11 00:39:20 +0700

    updet

--- js/controller/contactctrl.js
+++ js/controller/contactctrl.js
@@ -1,60 +1,4 @@
-// 1. Google Map // 
-
-
 am.controller('ContactCtrl', ['$scope', '$state', 'NgMap', function($scope, $state, NgMap){
-	 // Map Settings //
-  //   $scope.initialise = function() {
-  //   	if (navigator.geolocation) {
-		//         navigator.geolocation.getCurrentPosition(showPosition);
-		//     } else {
-		//         //browser does not support geolocation
-		//     }
-		// function showPosition(position) {
-		//     var latitude = position.coords.latitude;
-		//     var longitude = position.coords.longitude; 
-		// }
-
-  //       var myPos = new google.maps.LatLng(latitude,);
-  //       var mapOptions = {
-  //           center: myPos,
-  //           zoom: 16,
-  //           mapTypeId: google.maps.MapTypeId.ROADMAP
-  //       };
-  //       var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  //     // Geo Location /
-  //       navigator.geolocation.getCurrentPosition(function(pos) {
-  //           map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-  //           var myLocation = new google.maps.Marker({
-  //               position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-  //               map: map,
-  //               animation: google.maps.Animation.DROP,
-  //               title: "Lokasi Saya"
-  //           });
-  //       });
-  //       $scope.map = map;
-  //       // Additional Markers //
-  //       $scope.markers = [];
-  //       var infoWindow = new google.maps.InfoWindow();
-  //       var createMarker = function (info){
-  //           var marker = new google.maps.Marker({
-  //               position: new google.maps.LatLng(info.lat, info.long),
-  //               map: $scope.map,
-  //               animation: google.maps.Animation.DROP,
-  //               title: info.lks
-  //           });
-  //           marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-  //           google.maps.event.addListener(marker, 'click', function(){
-  //               infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-  //               infoWindow.open($scope.map, marker);
-  //           });
-  //           $scope.markers.push(marker);
-  //       }  
-  //       for (i = 0; i < cities.length; i++){
-  //           createMarker(cities[i]);
-  //       }
-
-  //   };
-  //   google.maps.event.addDomListener(document.getElementById("map"), 'load', $scope.initialise());
   $scope.init = function() {
     $scope.locations = [
         {
@@ -95,7 +39,7 @@ am.controller('ContactCtrl', ['$scope', '$state', 'NgMap', function($scope, $sta
         {
             lks : 'Anugerah Motor - BG Junction',
             desc : 'Test',
-            pos : [7.251574,  112.7337643]
+            pos : [-7.251574,  112.7337643]
         }
     ];
     $scope.loadMap();
