1229e298462f855ac5029c97a6ce2d16b8a73800
solrizal <rizal.solichudin@gmail.com>
2017-11-21 23:11:31 +0700

    commit

--- adm/js/app.js
+++ adm/js/app.js
@@ -1,4 +1,4 @@
-var am = angular.module('adm', ['ui.router', 'ui.bootstrap', 'ngMap']);
+var am = angular.module('adm', ['ui.router', 'ui.bootstrap', 'ngMap', 'ui.grid']);
 
 am.run(function($rootScope, $state, $ws) {
 	$rootScope.$on("$stateChangeSuccess", function(event, toState) {
