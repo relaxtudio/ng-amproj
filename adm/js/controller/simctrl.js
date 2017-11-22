9a04289df3d459621dbe813b44a8d655f5e9fe0a
solrizal <rizal.solichudin@gmail.com>
2017-11-22 17:41:17 +0700

    commit

--- adm/js/controller/simctrl.js
+++ adm/js/controller/simctrl.js
@@ -1,7 +1,12 @@
-am.controller('SimCtrl', function($scope, $state, $ws) {
+am.controller('SimCtrl', function($scope, $state, $ws, $interval) {
 	$scope.init = function() {
-		$scope.data = {};
-		$scope.gridBunga = {};
+		$scope.data = {
+			bunga: [],
+			fixcap: []
+		};
+		$scope.gridBunga = {
+			rowEditWaitInteval: -1
+		};
 		$scope.gridBunga.columnDefs = [
 			{name: 'id', enableCellEdit: false},
 			{name: 'thnmin'},
@@ -28,16 +33,22 @@ am.controller('SimCtrl', function($scope, $state, $ws) {
 	$scope.initWs = function() {
 		var token = $scope.$parent.user.token;
 		$ws.getValue({token: token}, function(respon) {
-			$scope.data.bunga = respon.data;
-			$scope.gridBunga.data = respon.data;
+			respon.data.forEach(function(value) {
+				$scope.gridBunga.data.push(value);
+				$scope.data.bunga.push(value);
+			})
 			$ws.getValue({token: token, data: {table: 'fixcap'}}, function(respon) {
 				$scope.data.fixcap = respon.data;
 				$scope.gridFixcap.data = respon.data;
-			})
+			}, error);
 		}, error);
 	}
 
-	
+	$scope.saveRow = function(data) {
+		var promise = ExpService.insertExp( rowEntity );
+		$scope.gridApi.rowEdit.setSavePromise( rowEntity, promise );
+		console.log(data.data, $scope.data);
+	}
 
 	$scope.init();
 })
