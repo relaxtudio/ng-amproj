4ad4774aa88843a2ca838cdf72147af29d3f2c6d
renanonymous21 <rendy.kr21@gmail.com>
2017-12-01 16:36:51 +0700

    akun

--- adm/js/controller/akunctrl.js
+++ adm/js/controller/akunctrl.js
@@ -1,3 +1,19 @@
-am.controller('AkunCtrl', ['$scope', '$state', '$ws', function($scope, $state, $ws) {
-	
+am.controller('AkunCtrl', ['$scope', '$state', '$ws', '$uibModal', '$uibModalStack', function($scope, $state, $ws, $uibModal, $uibModalStack) {
+	$scope.openModalAkun = function() {
+		var modal = $uibModal.open({
+			templateUrl: "template/modal/akunAdd.html",
+			scope: $scope
+		});
+	}
+
+	$scope.deleteAkun = function() {
+		var modal = $uibModal.open({
+			templateUrl: "template/modal/akunDelete.html",
+			scope: $scope
+		})
+	}
+
+	$scope.cancel = function() {
+		$uibModalStack.dismissAll();
+	}
 }])
